c8dc945035291cef700aeb745e725cf6d419d9b3
charlie <shinsc196@naver.com>
2021-03-18 11:25:33 +0900

    20210318

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+//import { render } from '@testing-library/react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="gray-background">
+          <img src={logo} lat="logo" />
+          <h2>Let's develop management system!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
