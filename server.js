1a69eba50ec850ee6d40ea8a57e1e22e7877af1c
charlie <shinsc196@naver.com>
2021-03-19 15:22:36 +0900

    server.js update

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.get('/api/hello', (req, res) => {
     res.send({message: 'Hello Express!'});
 });
 
-app.listen(port, () => console.log('Listening on port ${port}'));
+app.listen(port, () => console.log(`Listening on port ${port}`));
