b00e45e8d1c629f61b6bcd57f7284de6da3466ed
charlie <shinsc196@naver.com>
2021-03-19 10:17:40 +0900

    CreateMessage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-//import { render } from '@testing-library/react';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'http://placeimg.com/64/64/1',
+    'name': '선덕',
+    'birthday': '021222',
+    'gender': '여자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'http://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '011222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'http://placeimg.com/64/64/3',
+    'name': '허준',
+    'birthday': '991222',
+    'gender': '남자',
+    'job': '대학생'
+  }
+
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-          <img src={logo} lat="logo" />
-          <h2>Let's develop management system!</h2>
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              >
+              </Customer>
+            );
+          })
+        }
       </div>
-    );
+      );
   }
 }
 
